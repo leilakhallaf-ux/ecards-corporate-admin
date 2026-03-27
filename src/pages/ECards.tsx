03bbf80773d0bdb22ab3dc4d9f7a2fc1f70608d6
leilakhallaf-ux <leila.khallaf@manufactur-e.com>
2026-03-27 13:15:29 +0100

    Update ECards.tsx

--- src/pages/ECards.tsx
+++ src/pages/ECards.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useMemo } from 'react'
+import { useEffect, useState, useMemo } from 'react' 
 import { Link } from 'react-router-dom'
 import { supabase } from '../lib/supabase'
 import { ECard } from '../lib/types'
